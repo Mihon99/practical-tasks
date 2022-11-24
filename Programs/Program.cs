4bf97f2436b54387eaef99a18ccca927bef68ec4
reaper <reaper>
2022-11-24 20:07:41 +0300

    checking the condition

--- Programs/Program.cs
+++ Programs/Program.cs
@@ -1,4 +1,5 @@
-﻿string[] array = GetArray("Введите массив чисел: ");
+﻿string[] array1 = GetArray("Введите массив чисел: ");
+string[] array2 = new string[array1.Length];
 
 string[] GetArray(string message)
 {
@@ -6,4 +7,17 @@ string[] GetArray(string message)
     string getArray = Console.ReadLine();
     string[] array = getArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);    
     return array;
+}
+
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
 }
