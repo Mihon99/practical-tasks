642676a746724b1f8852e3518a5eb36837ee8272
reaper <reaper>
2022-11-24 20:05:27 +0300

    creat first array

---
+++ Programs/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] array = GetArray("Введите массив чисел: ");
+
+string[] GetArray(string message)
+{
+    Console.WriteLine(message);
+    string getArray = Console.ReadLine();
+    string[] array = getArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);    
+    return array;
+}
